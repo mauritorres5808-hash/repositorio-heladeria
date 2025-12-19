71a6238882a0dfb4d09a0533bfd8af8d50eabaff
Mauricio Torres <mauritorres5808@gmail.com>
2025-12-19 18:57:02 -0300

    Usar Firebase credentials por variables de entorno

--- server.js
+++ server.js
@@ -12,11 +12,14 @@ const path = require("path");
 // ==========================================
 //  Inicializar Firebase Admin
 // ==========================================
-const serviceAccount = require("./serviceAccountKey.json");
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  })
 });
+
 
 const db = admin.firestore();
 

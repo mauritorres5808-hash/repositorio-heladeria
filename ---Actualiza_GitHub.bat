2b008aaf0d83ee85e6c50f1ca3bba376f18a1018
Mauricio Torres <mauritorres5808@gmail.com>
2026-05-01 00:51:56 -0300

    actualizando la version

--- ---Actualiza_GitHub.bat
+++ ---Actualiza_GitHub.bat
@@ -1,3 +1,9 @@
-git add .
-git commit -m "actualizando la version"
-git push
+rem para actualizar TODO
+ git add .
+ git commit -m "actualizando la version"
+ git push
+
+rem para actualizar SOLO la carpeta img
+rem git add public/img
+rem git commit -m "Agrego imagenes de productos"
+rem git push

2b018e048ec8dc55d29a89520c535baef132a108
Mauricio Torres <mauritorres5808@gmail.com>
2026-05-23 20:39:34 -0300

    cambio api railway

--- PUBLIC/JS/config.js
+++ PUBLIC/JS/config.js
@@ -1,5 +1,5 @@
 const API_BASE = 
   location.hostname === "localhost"
     ? "http://localhost:3000"
-    : "https://whatsapp-backend-tp21.onrender.com";
+    : "https://repositorio-heladeria-production.up.railway.app";
 	

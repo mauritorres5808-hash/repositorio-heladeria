70696da9dbdf8bc920189edd18503a90d1f2aacf
Mauricio Torres <mauritorres5808@gmail.com>
2026-05-23 21:37:24 -0300

    fix dotenv production2

--- db.js
+++ db.js
@@ -1,3 +1,4 @@
+/*
 const mysql = require('mysql2/promise');
 
 if (process.env.NODE_ENV !== 'production') {
@@ -16,4 +17,26 @@ const pool = mysql.createPool({
 });
 
 module.exports = pool;
+*/
 
+
+const mysql = require('mysql2/promise');
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+const pool = mysql.createPool({
+
+  host: process.env.MYSQLHOST || process.env.DB_HOST,
+  port: process.env.MYSQLPORT || process.env.DB_PORT,
+  user: process.env.MYSQLUSER || process.env.DB_USER,
+  password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
+  database: process.env.MYSQLDATABASE || process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+
+});
+
+module.exports = pool;

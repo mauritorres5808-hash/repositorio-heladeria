d02766a471ff6457019dca1b3dfb7e21b93e221a
Mauricio Torres <mauritorres5808@gmail.com>
2026-05-23 22:44:32 -0300

    debug railway env

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
+
 const mysql = require('mysql2/promise');
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
 
 const pool = mysql.createPool({
 
